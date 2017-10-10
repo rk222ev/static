f4742036aa718e8aa724cbf25fb01fe48fd3d384
Robin Karlsson <robinpkarlsson@gmail.com>
2017-10-10 21:12:06 +0200

    Update dependecies using lein-ancient

--- project.clj
+++ project.clj
@@ -2,15 +2,16 @@
   :description "Simple static site generator."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [cssgen "0.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [hiccup "1.0.5"]
                  [org.pegdown/pegdown "1.6.0"]
-                 [ring/ring-core "1.5.1"]
-                 [ring/ring-jetty-adapter "1.5.1"]
+                 [ring/ring-core "1.6.2"]
+                 [ring/ring-jetty-adapter "1.6.2"]
                  [watchtower "0.1.1"]
                  [stringtemplate-clj "0.1.0"]
-                 [org.clojure/core.memoize "0.5.8"]]
+                 [org.clojure/core.memoize "0.5.9"]]
+  :profiles {:dev {:plugins [[lein-ancient "0.6.12"]]}}
   :main static.core
   :aot :all
   :jar-name "static.jar"
